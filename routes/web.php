55cb3dec66dbf983a7307c72517094c2d0c99b84
3A432055 <david860120@gmail.com>
2019-10-23 10:48:34 +0800

    設定 Route 反應

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,13 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/',function(){
+    return'welcome';
+});
+
+
+Route::get('/',function(){
+    returnredirect('welcome');
+});
+
